b0767233a16e31aae28284b1020520be63c4579f
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-08-18 22:57:55 -0300

    feat: transform game class in a table on database

--- src/main/java/com/alejuniordev/dslist/entities/Game.java
+++ src/main/java/com/alejuniordev/dslist/entities/Game.java
@@ -1,10 +1,19 @@
 package com.alejuniordev.dslist.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "tb_game")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platform;
@@ -13,7 +22,11 @@ public class Game {
     private String shortDescription;
     private String longDescription;
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game() {
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+                String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
