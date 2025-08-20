de02169791e91de838a82650c5294dbfbabfd60b
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-08-20 15:55:48 -0300

    feat: create controller to find game by id

--- src/main/java/com/alejuniordev/dslist/services/GameService.java
+++ src/main/java/com/alejuniordev/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.alejuniordev.dslist.services;
 
+import com.alejuniordev.dslist.dtos.GameDTO;
 import com.alejuniordev.dslist.dtos.GameMinDTO;
 import com.alejuniordev.dslist.entities.Game;
 import com.alejuniordev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,19 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    // @Transactional -> Assegura que essa função não será utilizada para escrita
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(item -> new GameMinDTO(item)).toList();
+
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
 
         return dto;
     }
