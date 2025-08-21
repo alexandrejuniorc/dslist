2a191e3d9777883298a272a771e434ccb8c89a34
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-08-20 23:07:15 -0300

    feat: create game list controller using customized sql query

--- src/main/java/com/alejuniordev/dslist/dtos/GameMinDTO.java
+++ src/main/java/com/alejuniordev/dslist/dtos/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.alejuniordev.dslist.dtos;
 
 import com.alejuniordev.dslist.entities.Game;
+import com.alejuniordev.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -18,6 +19,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/alejuniordev/dslist/services/GameService.java
+++ src/main/java/com/alejuniordev/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.alejuniordev.dslist.services;
 import com.alejuniordev.dslist.dtos.GameDTO;
 import com.alejuniordev.dslist.dtos.GameMinDTO;
 import com.alejuniordev.dslist.entities.Game;
+import com.alejuniordev.dslist.projections.GameMinProjection;
 import com.alejuniordev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,14 @@ public class GameService {
     public GameDTO findById(Long id) {
         Game result = gameRepository.findById(id).get();
         GameDTO dto = new GameDTO(result);
+
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(item -> new GameMinDTO(item)).toList();
 
         return dto;
     }
